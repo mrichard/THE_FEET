2cb89889dfe1aa85ee8c0c5988a8c658471fdde3
Michel Richard <mike_r1978_6@hotmail.com>
2012-05-22 20:37:34 -0400

    ...

--- app/main.rb
+++ app/main.rb
@@ -13,10 +13,31 @@ end
 #  File.read(File.join('public', 'html/home.html'))
 #end
 
+DB = Mongo::Connection.new.db("mydb", :pool_size => 5, :timeout => 5)
+
+# HOME PAGE
 get '/entry' do
   haml :'entry/entry'
 end
 
+# INFOMATIONAL CODE-CONVENTIONS
 get '/code-conventions' do
   haml :'codeconventions/code'
 end
+
+# ARTICLE PAGE
+get '/news/articles' do  
+  # list all articles available  
+end  
+get '/news/article/:id' do  
+  # get a single article  
+end  
+post '/news/article' do  
+  # create a new article  
+end  
+put '/news/article/:id' do  
+  # update an existing article  
+end  
+delete '/news/article/:id' do  
+  # delete an article  
+end  
